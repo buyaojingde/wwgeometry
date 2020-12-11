9a5b43200afceaca33cb70d11115299367664a4b
guolianbo <guolianbo@countrygarden.com.cn>
2020-12-11 17:16:42 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,14 +20,14 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    quotes: [2, "single", { avoidEscape: true }],
+    quotes: [0, "single", { avoidEscape: true }],
   },
 
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
         jest: true,
